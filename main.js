ad51f7620e27e48c7b988d18aaafccf88b8032e4
Lê Thanh Phương <98444115+Kouro2510@users.noreply.github.com>
2023-03-24 13:58:10 +0700

    Update

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
-var dropdowns = document.querySelectorAll('.dropdown');
-for (var i = 0; i < dropdowns.length; i++) {
-    dropdowns[i].addEventListener('click', function() {
-        this.classList.toggle('active');
+function myFunction() {
+    document.getElementById("Dropdown").classList.toggle("active");
+}
+
+//lấy tất cả các button menu
+var buttons = document.getElementsByClassName('dropdown');
+//lấy tất cả các thẻ chứa menu con
+var contents = document.getElementsByClassName('dropdown-menu');
+//lặp qua tất cả các button menu và gán sự kiện
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        //lấy value của button
+        var id = this.value;
+        //ẩn tất cả các menu con đang được hiển thị
+        for (var i = 0; i < contents.length; i++) {
+            contents[i].classList.remove("active");
+        }
+        //hiển thị menu vừa được click
+        myFunction();
     });
 }
+window.addEventListener("click", function(event) {
+    var dropdowns = document.getElementsByClassName("dropdown");
+    for (var i = 0; i < dropdowns.length; i++) {
+        if (dropdowns[i].contains(event.target)) {
+            // clicked element is a child of a dropdown menu, do nothing
+            return;
+        }
+        dropdowns[i].classList.remove("active");
+    }
+
+});
+
 const currentDate = new Date();//Lấy ngày tháng năm hiện tại
 const currentYear = currentDate.getFullYear();//Lấy năm
 
 const yearElement = document.getElementById("timenow"); // Lấy phần tử HTML muốn hiển thị năm vào
 yearElement.innerHTML = ` My Website &copy; ${currentYear} `;//Set chữ vào nơi có id là Time now
-let slideIndex = 0;
-showSlides();
+
 function showSlides() {
     const carousel = document.querySelector('.carousel');
     const slides = carousel.querySelectorAll('.slide');
@@ -19,8 +45,13 @@ function showSlides() {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 5000);
 }
+
+let slideIndex = 0;
+showSlides();
 
