faf853a8d6682671399679300300cd272ac93cb3
Lê Thanh Phương <98444115+Kouro2510@users.noreply.github.com>
2023-03-28 13:57:07 +0700

    Comple checkout.html css 610 - 789

--- modal.js
+++ modal.js
@@ -46,3 +46,8 @@ openModalBtn.addEventListener("click", function() {
 closeModalBtn.addEventListener("click", function() {
     modal.style.display = "none";
 });
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
